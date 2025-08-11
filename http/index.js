abe27293e1beb7c877e731445592cc78ce28737e
Shreyanshsingh23 <shreyanshrewa@gmail.com>
2025-08-11 12:02:08 +0530

    Implement request logging and response handling in the server

--- http/index.js
+++ http/index.js
@@ -1,9 +1,33 @@
 const http = require('http')
+const fs = require('fs')
 
 const myServer =  http.createServer((req, res) => {
-        console.log('New Request');
-        res.end('New Response Sent')
+        let log = `${Date.now()} : ${req.url} New Request Received\n`
+        fs.appendFile('log.txt', log, (err, data) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('Log Updated')
+            }
+        })
+        switch(req.url) {
+    case '/':
+        res.end('<h1>Home Page</h1>')
+        break
+    case '/about':
+        res.end('<h1>About Page</h1>')
+        break
+    case '/contact':
+        res.end('<h1>Contact Page Data</h1>')
+        break
+    default:
+        res.write('<h1>404 Page Not Found</h1>')
+        break
+}
     })
+
+
+
 
 myServer.listen(8000, () => {
     console.log('Server is listening on port 8000')
